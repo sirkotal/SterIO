b42f7a60b7281c9030732bee8ce82a58a90039e1
sirkotal <lordkotal2002@gmail.com>
2023-01-16 12:36:48 +0000

    Added Player

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ client.once('ready', () => {
    
    client.on("error", console.error);
    client.on("warn", console.warn);
+
+const player = new Player(client);
+
+player.on("error", (queue, error) => {
+    console.log(`[${queue.guild.name}] Error stemmed from the queue: ${error.message}`);
+});
+player.on("connectionError", (queue, error) => {
+    console.log(`[${queue.guild.name}] Error stemmed from the connection: ${error.message}`);
+});
+
+player.on("trackStart", (queue, track) => {
+    queue.metadata.send(`🎶 | Now playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+});
+
+player.on("trackAdd", (queue, track) => {
+    queue.metadata.send(`🎶 | Track **${track.title}** has been queued!`);
+});
+
+player.on("botDisconnect", (queue) => {
+    queue.metadata.send("❌ | Hasta la vista, baby! Clearing queue!");
+});
+
+player.on("channelEmpty", (queue) => {
+    queue.metadata.send("❌ | Can't party on my own, leaving...");
+});
+
+player.on("queueEnd", (queue) => {
+    queue.metadata.send("✅ | Queue has finished!");
+});
